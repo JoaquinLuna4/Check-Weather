7a669108b5e491b3be92082e780bc62487de8bac
joacoluna4 <joacolunaf4@gmail.com>
2021-12-28 12:31:35 -0300

    Se consigue vincular API mediante hooks

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,65 +4,16 @@ import PreviewPic from "./preview";
 import Type from "./type";
 import Grade from "./grade";
 
-const API =
-	"https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=dcd20e9b46ed770b171e69f37ee13d57";
-const DEFAULT_QUERY = "redux";
+// const API =
+// 	"https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=dcd20e9b46ed770b171e69f37ee13d57";
+// const DEFAULT_QUERY = "redux";
 
 class Resume extends Component {
-	// state = {
-	// 	data: [
-	// 		{
-	// 			id: 245722,
-	// 			weather_state_name: "Light Cloud",
-	// 			weather_state_abbr: "lc",
-	// 			wind_direction_compass: "NE",
-	// 			created: "2013-04-23T05:47:24.233130Z",
-	// 			applicable_date: "2013-04-27",
-	// 			min_temp: 17,
-	// 			max_temp: 23,
-	// 			the_temp: null,
-	// 			wind_speed: 8,
-	// 			wind_direction: 44.99999999999999,
-	// 			air_pressure: null,
-	// 			humidity: null,
-	// 			visibility: null,
-	// 			predictability: 70,
-	// 		},
-	// 	],
-	// };
-
-	//FORMATO YT
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			data: {
-				coord: {},
-				weather: [
-					{
-						description: null,
-						base: null,
-					},
-				],
-				base: [],
-				main: {},
-				visibility: [],
-				wind: [],
-				clouds: [],
-				dt: [],
-				sys: [],
-				timezone: [],
-				id: [],
-				name: [],
-				cod: [],
-			},
-		};
-	}
-	componentDidMount() {
-		fetch(API + DEFAULT_QUERY)
-			.then((response) => response.json())
-			.then((dataJson) => this.setState({ data: dataJson.data }));
-	}
+	// componentDidMount() {
+	// 	fetch(API + DEFAULT_QUERY)
+	// 		.then((response) => response.json())
+	// 		.then((dataJson) => this.setState({ data: dataJson.data }));
+	// }
 	// componentDidMount() {
 	// 	fetch(API + DEFAULT_QUERY)
 	// 		.then((response) => response.json())
@@ -85,31 +36,26 @@ class Resume extends Component {
 	// };
 
 	render() {
-		const { data } = this.state;
 		return (
 			<React.Fragment>
 				<div className="container-resume">
-					{/* {weather.map((e, i) => {
-						return <Type name={e.description} key={i} />;
-					})} */}
-
-					{/* <PreviewPic
+					<PreviewPic
 						state="https://images.vexels.com/media/users/3/234492/isolated/lists/03325d6a72e4f878170e0076f08bab39-cielo-de-tiempo-en-la-nube.png"
 						alt="Preview image from weather"
 					/>
 					<Grade number="15 °C" />
-					<Type name={"shower"} />
-					<Date today="Fri, 5 Jun" /> */}
+					<Type />
+					<Date today="Fri, 5 Jun" />
 
 					{/* EXAMPLE FETCH DATA FUNCIONA */}
-					<ul>
+					{/* <ul>
 						{data.map((e) => (
 							<li key={e.id}>
 								<a href={e.url}>{e.title}</a>
 								{console.log(e.title)}
 							</li>
 						))}
-					</ul>
+					</ul> */}
 				</div>
 			</React.Fragment>
 		);
--- src/Components/type.js
+++ src/Components/type.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-class Type extends React.Component {
-	render() {
-		return (
-			<React.Fragment>
-				<h2 className="center type-value">{this.props.name}</h2>
-			</React.Fragment>
-		);
-	}
-}
+const Type = () => {
+	const API =
+		"https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=dcd20e9b46ed770b171e69f37ee13d57";
+	// const API = "https://jsonplaceholder.typicode.com/users";
+	const [est, setEst] = React.useState([]);
+	React.useEffect(() => {
+		apiGet();
+	}, []);
+
+	const apiGet = async () => {
+		const data = await fetch(API);
+		const users = await data.json();
+		setEst(users);
+	};
+	return (
+		<React.Fragment>
+			{/* <h2 className="center type-value">Shower</h2> */}
+			<h2 className="center type-value">{est.weather[0].main}</h2>
+		</React.Fragment>
+	);
+};
+
 export default Type;
