9b6c1c0fd17b508151ebc91b2e8a2f278d2dc8d7
joacoluna4 <joacolunaf4@gmail.com>
2021-12-28 14:10:20 -0300

    Modif de estilos y datos de date component

--- src/Components/date.js
+++ src/Components/date.js
@@ -1,19 +1,36 @@
 import React from "react";
 
-class Date extends React.Component {
-	state = {};
-	render() {
-		return (
-			<React.Fragment>
-				<div className="containerDate">
-					<h4 className="center">Today</h4>
-					<h4 className="center"> . </h4>
-					<h4 className="center">{this.props.today}</h4>
-					{/* METER ESTO EN GRID SERA MAS
+const Date = () => {
+	const API =
+		"https://api.openweathermap.org/data/2.5/weather?q=cordoba&appid=dcd20e9b46ed770b171e69f37ee13d57";
+
+	const [est, setEst] = React.useState([]);
+
+	React.useEffect(() => {
+		apiGet();
+	}, []);
+
+	const apiGet = async () => {
+		const data = await fetch(API);
+		const users = await data.json();
+		setEst(users);
+	};
+
+	return (
+		<React.Fragment>
+			<div className="containerDate">
+				<h4 className="center">Today</h4>
+				<h4 className="center"> . </h4>
+				<h4 className="center">14 jul</h4>
+				<footer className="center footerdate">
+					{/* <span class="material-icons-outlined">place</span> */}
+					{/* <h4 className="center">Córdoba</h4> */}
+					<h4 className="center">{est.name}</h4>
+				</footer>
+				{/* METER ESTO EN GRID SERA MAS
 					FACIL */}
-				</div>
-			</React.Fragment>
-		);
-	}
-}
+			</div>
+		</React.Fragment>
+	);
+};
 export default Date;
--- src/Components/type.js
+++ src/Components/type.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Type = () => {
 	const API =
-		"https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=dcd20e9b46ed770b171e69f37ee13d57";
+		"https://api.openweathermap.org/data/2.5/weather?q=cordoba&appid=dcd20e9b46ed770b171e69f37ee13d57";
 	// const API = "https://jsonplaceholder.typicode.com/users";
 	const [est, setEst] = React.useState([]);
+	// let carga;
 	React.useEffect(() => {
 		apiGet();
+		// setEst((carga = true));
 	}, []);
 
 	const apiGet = async () => {
 		const data = await fetch(API);
 		const users = await data.json();
 		setEst(users);
+		// setEst((carga = false));
 	};
 	return (
 		<React.Fragment>
 			{/* <h2 className="center type-value">Shower</h2> */}
-			<h2 className="center type-value">{est.weather[0].main}</h2>
+			<h2 className="center type-value">{est.weather[0].description}</h2>
 		</React.Fragment>
 	);
 };
