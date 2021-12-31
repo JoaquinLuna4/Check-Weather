76990b34d34311c7a501fae9fc1b85e9853b1d57
joacoluna4 <joacolunaf4@gmail.com>
2021-12-31 12:54:46 -0300

    Solucionando problemas de carga de API

--- src/Components/date.js
+++ src/Components/date.js
@@ -1,12 +1,20 @@
 import React from "react";
-
+import { Spinner } from "react-bootstrap";
 const Date = () => {
 	const API =
 		"https://api.openweathermap.org/data/2.5/weather?q=cordoba&appid=dcd20e9b46ed770b171e69f37ee13d57";
 
 	const [est, setEst] = React.useState([]);
+	const [isLoading, setLoading] = React.useState(true);
+	console.log(est, "EST INICIO");
+	console.log(isLoading, "isLoading INICIO");
 
 	React.useEffect(() => {
+		fetch(API).then((users) => {
+			setLoading(false);
+			setEst(users);
+			console.log(isLoading, "debe ser false");
+		});
 		apiGet();
 	}, []);
 
@@ -14,21 +22,29 @@ const Date = () => {
 		const data = await fetch(API);
 		const users = await data.json();
 		setEst(users);
+		setLoading(false);
 	};
 
 	return (
 		<React.Fragment>
 			<div className="containerDate">
-				<h4 className="center">Today</h4>
-				<h4 className="center"> . </h4>
-				<h4 className="center">14 jul</h4>
+				<div className="containerDateInfo center">
+					<h4 className="center pad-right10">Today</h4>
+
+					<h4 className="center pad-right10 mar-top16"> . </h4>
+
+					<h4 className="center pad-right10">14 jul</h4>
+				</div>
 				<footer className="center footerdate">
-					{/* <span class="material-icons-outlined">place</span> */}
+					<img src=".\images\location_on_white_24dp.svg" alt="icon" />
+
 					{/* <h4 className="center">Córdoba</h4> */}
-					<h4 className="center">{est.name}</h4>
+					{isLoading ? (
+						<h4 className="center">Cargando</h4>
+					) : (
+						<h4 className="center">{est.name}</h4>
+					)}
 				</footer>
-				{/* METER ESTO EN GRID SERA MAS
-					FACIL */}
 			</div>
 		</React.Fragment>
 	);
--- src/Components/grade.js
+++ src/Components/grade.js
@@ -4,9 +4,17 @@ const Grade = () => {
 	const API =
 		"https://api.openweathermap.org/data/2.5/weather?q=cordoba&units=metric&appid=dcd20e9b46ed770b171e69f37ee13d57&lang=es";
 	const [est, setEst] = React.useState([]);
-	const [isLoading, setLoading] = React.useState(false);
 
+	const [isLoading, setLoading] = React.useState(true);
+	console.log(isLoading, "el loading abajo del estate");
+	console.log(est, "el est abajo del true");
 	React.useEffect(() => {
+		setLoading(true);
+		fetch(API).then((users) => {
+			setLoading(false);
+			setEst(false);
+			console.log(isLoading, "debe ser false");
+		});
 		apiGet();
 	}, []);
 
@@ -14,15 +22,18 @@ const Grade = () => {
 		const data = await fetch(API);
 		const users = await data.json();
 		setEst(users);
-		setLoading(true);
+		setLoading(false);
 	};
 	return (
 		<React.Fragment>
 			{isLoading ? (
-				<h2 className="center grade-value">CARGANDO</h2>
+				<h2 className="center grade-value">CARGANDO CARNAL</h2>
 			) : (
 				<h2 className="center grade-value">{Math.trunc(est.main.temp)}°C</h2>
 			)}
+			{/* <h2 className="center grade-value">CARGANDO CARNAL</h2> */}
+
+			{/* <h2 className="center grade-value">{Math.trunc(est.main.temp)}°C</h2> */}
 		</React.Fragment>
 	);
 };
